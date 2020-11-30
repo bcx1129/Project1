9486d578b5ce26df46b991e76b13e76e96e6db8c
bcx1129A <1160549250@qq.com>
2020-12-01 00:20:37 +0800

    12323

---
+++ Module01/src/com/autozi/test1/Test.java
@@ -0,0 +1,14 @@
+package com.autozi.test1;
+
+/**
+ * asdfadsfdasfasdf
+ */
+public class Test
+{
+    public static void main(String[] args) {
+        System.out.println("231" +
+                "213" +
+                "46545645");
+        System.out.println("23132132123");
+    }
+}
